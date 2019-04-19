751c64d8a43f57dabf069c26c82308c464747d8c
Kelvin <admin@maydear.com>
2019-04-19 16:10:26 +0800

    删除

--- tests3/sqlserver.py
+++
@@ -1,8 +0,0 @@
-import pyodbc
-
-conn  = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=tcp:yunzhucsdev.database.chinacloudapi.cn,1433;UID=yzsqlcsdev;PWD=tz5566_6633;Database=System;')
-cursor = conn.cursor()
-cursor.execute('select * from SYSTEM_FEATURE')
-rows = cursor.fetchall()
-for row in rows:
-    print(row,1)
